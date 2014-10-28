84c81077b1eb1277da4c5b10027c5a187282507e
jeffcampomanes <jeffcampomanes@me.com>
2014-10-28 18:58:47 -0400

    my first heroku commit

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,30 @@
+class UsersController < ApplicationController
+
+  def index
+    @users = User.all
+  end
+
+	def new 
+		@user = User.new
+	end
+
+	def create
+		@user = User.new(
+			first_name: params[:first_name],
+			last_name: params[:last_name],
+			email: params[:email], 
+			password: params["password"],
+			gender: params["gender"]
+			)
+		if @user.save
+			redirect_to '/login'
+		end
+	end
+
+  def update
+    @user = User.find(params[:id])
+  end
+
+
+###  
+end
